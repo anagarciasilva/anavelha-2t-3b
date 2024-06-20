a3aa45972772b2c059c9a66e5d2194c748ceacbd
anagarciasilva <159449545+anagarciasilva@users.noreply.github.com>
2024-06-20 16:25:46 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -19,54 +19,54 @@ const perguntas = [
         ]
     },
     {
-        enunciado: "Você considera alguma  amizade igual a amizade do Patrick com o Bob esponja?",
+        enunciado: "Qual das irmãs era mais velha?",
         alternativas: [
             {
-                texto: "não, acho a amizade desles estranha!",
-                afirmacao: "Conseguiu utilizar a IA para buscar informações úteis."
+                texto: "Anna",
+                afirmacao: "Anna era mais nova"
             },
             {
-                texto: "sim, adoro a sincronização deles!",
-                afirmacao: "Sentiu mais facilidade em utilizar seus próprios recursos para escrever seu trabalho."
+                texto: "Elsa",
+                afirmacao: "Elsa era mais velha"
             }
         ]
     },
     {
-        enunciado: "Após a elaboração do trabalho escrito, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
+        enunciado: "Qual magia a Elsa tinha?",
         alternativas: [
             {
-                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
-                afirmacao: "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA."
+                texto: "gelo",
+                afirmacao: "Sim, ela tinha magia de gelo, por isso usava luvas"
             },
             {
-                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
-                afirmacao: "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética."
+                texto: "fogo",
+                afirmacao: "não,ela tinha magia de gela"
             }
         ]
     },
     {
-        enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
+        enunciado: "As irmas brigavam muito?",
         alternativas: [
             {
-                texto: "Criar uma imagem utilizando uma plataforma de design como o Paint.",
-                afirmacao: "Notou também que muitas pessoas não sabem ainda utilizar as ferramentas tradicionais e decidiu compartilhar seus conhecimentos de design utilizando ferramentas de pintura digital para iniciantes."
+                texto: "sim",
+                afirmacao: "quando ficaram adolecentes sim,pois Elsa não tinha muito paciencia"
             },
             {
-                texto: "Criar uma imagem utilizando um gerador de imagem de IA.",
-                afirmacao: "Acelerou o processo de criação de trabalhos utilizando geradores de imagem e agora consegue ensinar pessoas que sentem dificuldades em desenhar manualmente como utilizar também!"
+                texto: "nao",
+                afirmacao: "Elas brigavam bastante"
             }
         ]
     },
     {
-        enunciado: "Você tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda da IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz? ",
+        enunciado: "qual irmã se casou primeiro?",
         alternativas: [
             {
-                texto: "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
-                afirmacao: "Infelizmente passou a utilizar a IA para fazer todas suas tarefas e agora se sente dependente da IA para tudo."
+                texto: "Anna",
+                afirmacao: "Sim, foi a Anna"
             },
             {
-                texto: "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial.",
-                afirmacao: "Percebeu que toda IA reproduz orientações baseadas na empresa que programou e muito do que o chat escrevia não refletia o que pensava e por isso sabe que os textos gerados pela IA devem servir como auxílio e não resultado final. "
+                texto: "Elsa",
+                afirmacao: "Elsa nunca quis saber de namorado. "
             }
         ]
     },
@@ -105,7 +105,7 @@ function respostaSelecionada(opcaoSelecionada) {
 }
 
 function mostraResultado() {
-    caixaPerguntas.textContent = "Em 2049...";
+    caixaPerguntas.textContent = "No final de tudo..";
     textoResultado.textContent = historiaFinal;
     caixaAlternativas.textContent = "";
 }
